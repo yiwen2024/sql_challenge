f619cd75d8b073e15aa9256365cd7b06c74ac8b1
yiwen2024 <ywen299@outlook.com>
2024-05-06 22:49:40 -0400

    add

--- Employee_sql/Employee_query.sql
+++ Employee_sql/Employee_query.sql
@@ -1,49 +1,46 @@
-
---List the employee number, last name, first name, sex, and salary of each employee.
+--1.List the employee number, last name, first name, sex, and salary of each employee.
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 JOIN salaries ON employees.emp_no = salaries.emp_no;
 
---List the first name, last name, and hire date for the employees who were hired in 1986.
+--2. List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE hire_date LIKE '%1986';
+WHERE hire_date BETWEEN '01/01/1986' AND '12/31/1986';
 
---List the manager of each department along with their department number, department name, employee number, last name, and first name.
-SELECT dept_manager.dept_no, dept_manager.name, dept_manager.emp_no, employees.last_name, employees.first_name
+--3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
 FROM dept_manager
-JOIN departments ON dept_manager.dept_no = deparments.dept_no
-JOIN employees ON dept_manader.emp_no = employees.emp_no
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+JOIN employees ON dept_manager.emp_no = employees.emp_no;
 
---List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+--4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name, dept_emp.dept_no
-FROM employees
-JOIN employees ON deparment.emp_no = employees.emp_no
-JOIN dept_emp ON department.dept_no = dept_emp.dept_no
+FROM dept_emp
+JOIN employees ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON departments.dept_no = dept_emp.dept_no;
 
---List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+--5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 SELECT employees.first_name, employees.last_name, employees.sex
 FROM employees
-WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
---List each employee in the Sales department, including their employee number, last name, and first name.
+--6. List each employee in the Sales department, including their employee number, last name, and first name.
 -- The department number of Sales department is d007
-SELECT * FROM departments;
 SELECT employees.emp_no, employees.last_name, employees.first_name
 FROM employees
 JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-WHERE departments.dept_no = 'd007';
+WHERE dept_emp.dept_no = 'd007';
 
---List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+--7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 -- The department numbers of Sales and Development departments are d007 and d005
-SELECT * FROM departments;
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM employees
 JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_no = 'd007' or departments.dept_no = 'd005';
 
---List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+--8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT last_name, COUNT(last_name) AS "Frequency count"
 FROM employees
 GROUP BY last_name
--- Employee_sql/Table_schemata.sql
+++ Employee_sql/Table_schemata.sql
@@ -2,19 +2,19 @@
 
 --titles
 CREATE TABLE titles (
-    title_id VARCHAR   NOT NULL,
-    title VARCHAR   NOT NULL,
+    title_id VARCHAR(5)   NOT NULL,
+    title VARCHAR(20)   NOT NULL,
     PRIMARY KEY (title_id)
 );
 
 --employees
 CREATE TABLE employees (
-    emp_no INT   NOT NULL,
-    emp_title_id VARCHAR NOT NULL,
+    emp_no INT(5)   NOT NULL,
+    emp_title_id VARCHAR(5) NOT NULL,
     birth_date DATE   NOT NULL,
-    first_name VARCHAR   NOT NULL,
-    last_name VARCHAR   NOT NULL,
-    sex VARCHAR   NOT NULL,
+    first_name VARCHAR(20)   NOT NULL,
+    last_name VARCHAR(20)   NOT NULL,
+    sex VARCHAR(1)   NOT NULL,
     hire_date DATE   NOT NULL,
     FOREIGN KEY (emp_title_id) REFERENCES titles (title_id),
     PRIMARY KEY (emp_no)
@@ -22,15 +22,15 @@ CREATE TABLE employees (
 
 --departments
 CREATE TABLE departments (
-    dept_no VARCHAR   NOT NULL,
-    dept_name VARCHAR   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL,
+    dept_name VARCHAR (20)   NOT NULL,
     PRIMARY KEY (dept_no)
 );
 
 --dept_manager
 CREATE TABLE dept_manager (
-    dept_no VARCHAR   NOT NULL,
-    emp_no INT   NOT NULL,
+    dept_no VARCHAR(4)  NOT NULL,
+    emp_no INT(5)   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (dept_no, emp_no)
@@ -38,8 +38,8 @@ CREATE TABLE dept_manager (
 
 --dept_emp
 CREATE TABLE dept_emp (
-    emp_no INT   NOT NULL,
-    dept_no VARCHAR   NOT NULL,
+    emp_no INT(5)   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
@@ -47,8 +47,8 @@ CREATE TABLE dept_emp (
 
 --salaries
 CREATE TABLE salaries (
-    emp_no INT   NOT NULL,
-    salary INT   NOT NULL,
+    emp_no INT(5)  NOT NULL,
+    salary INT(6)   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
