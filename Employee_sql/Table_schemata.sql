5c08adda3aa9cd2e4cdea70f22475b0a0925b654
yiwen2024 <ywen299@outlook.com>
2024-05-06 23:13:10 -0400

    add

--- Employee_sql/Table_schemata.sql
+++ Employee_sql/Table_schemata.sql
@@ -9,7 +9,7 @@ CREATE TABLE titles (
 
 --employees
 CREATE TABLE employees (
-    emp_no INT(5)   NOT NULL,
+    emp_no INT   NOT NULL,
     emp_title_id VARCHAR(5) NOT NULL,
     birth_date DATE   NOT NULL,
     first_name VARCHAR(20)   NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE departments (
 --dept_manager
 CREATE TABLE dept_manager (
     dept_no VARCHAR(4)  NOT NULL,
-    emp_no INT(5)   NOT NULL,
+    emp_no INT   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (dept_no, emp_no)
@@ -38,7 +38,7 @@ CREATE TABLE dept_manager (
 
 --dept_emp
 CREATE TABLE dept_emp (
-    emp_no INT(5)   NOT NULL,
+    emp_no INT   NOT NULL,
     dept_no VARCHAR(4)   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
@@ -47,8 +47,8 @@ CREATE TABLE dept_emp (
 
 --salaries
 CREATE TABLE salaries (
-    emp_no INT(5)  NOT NULL,
-    salary INT(6)   NOT NULL,
+    emp_no INT  NOT NULL,
+    salary INT   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
